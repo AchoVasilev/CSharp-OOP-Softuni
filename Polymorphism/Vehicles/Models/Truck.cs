46cc225d9500678264721daaf9ed2e33b4f0f318
AngelVasilev <58532362+AchoVasilev@users.noreply.github.com>
2020-07-19 20:45:54 +0300

    Add files via upload

---
+++ Polymorphism/Vehicles/Models/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Models
+{
+    public class Truck : Vehicle
+    {
+        private const double DefaultAirConditionerFuelConsumption = 1.6;
+        private const double RefuelPercentage = 0.95;
+        public Truck(double fuelQuantity, double fuelConsumption, bool hasAirConditioner = true) 
+            : base(fuelQuantity, fuelConsumption, hasAirConditioner)
+        {
+        }
+
+        public override double AirConditionerFuelConsumption => DefaultAirConditionerFuelConsumption;
+
+        public override void Refuel(double liters)
+        {
+            base.Refuel(liters * RefuelPercentage);
+        }
+    }
+}
---
+++ Polymorphism/Vehicles/Models/Vehicle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Models
+{
+    public abstract class Vehicle : IVehicle
+    {
+        protected Vehicle(double fuelQuantity, double fuelConsumption, bool hasAirConditioner = true)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+            this.HasAirConditioner = hasAirConditioner;
+        }
+        public double FuelQuantity { get; protected set; }
+
+        public double FuelConsumption { get; }
+
+        public bool HasAirConditioner { get; }
+
+        public abstract double AirConditionerFuelConsumption { get; }
+
+        public bool Drive(double distance)
+        {
+            double spentFuel = distance * this.FuelConsumption;
+
+            if (HasAirConditioner)
+            {
+                spentFuel += AirConditionerFuelConsumption * distance;
+            }
+
+            if (this.FuelQuantity < spentFuel)
+            {
+                return false;
+            }
+
+            this.FuelQuantity -= spentFuel;
+            return true;
+        }
+
+        public virtual void Refuel(double liters)
+        {
+            this.FuelQuantity += liters;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {this.FuelQuantity:F2}";
+        }
+    }
+}
