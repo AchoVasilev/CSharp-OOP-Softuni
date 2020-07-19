4811188850aabf79694e6816260d597d161d8a17
AngelVasilev <58532362+AchoVasilev@users.noreply.github.com>
2020-07-19 20:45:12 +0300

    Create Car.cs

---
+++ Polymorphism/Vehicles/Models/Car.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Models
+{
+    public class Car : Vehicle
+    {
+        private const double DefaultAirConditionerFuelConsumption = 0.9;
+        public Car(double fuelQuantity, double fuelConsumption, bool hasAirConditioner = true) 
+            : base(fuelQuantity, fuelConsumption, hasAirConditioner)
+        {
+        }
+
+        public override double AirConditionerFuelConsumption => DefaultAirConditionerFuelConsumption;
+    }
+}
